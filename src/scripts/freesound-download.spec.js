661024bf1716a855eba357488bf768879c13d051
RicardoSilva91 <ricardos8977@gmail.com>
2022-08-09 13:53:23 +0100

    fix: fixes

--- src/scripts/freesound-download.spec.js
+++ src/scripts/freesound-download.spec.js
@@ -1,9 +1,65 @@
-const { test } = require('@playwright/test');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-test('basic test', async ({ page }) => {
+const { test, expect } = require('@playwright/test');
+const path = require('path');
+const { readFile, writeFile } = require('../utils/fs.utils');
+const { downloadFromPages, downloadItem } = require('../utils/browser.utils');
+
+test('basic test', async ({ page, context }) => {
   // Go to https://freesound.org/
   await page.goto('https://freesound.org/');
+
+  const shouldLogin = true;
+
+  if (shouldLogin) {
+    // Click text=Log In
+    await page.locator('text=Log In').click();
+    await expect(page).toHaveURL('https://freesound.org/home/login/?next=/');
+
+    await page.locator('input[id="id_username"]').click();
+
+    await page.locator('input[id="id_username"]').fill(process.env.FREESOUND_EMAIL);
+
+    await page.locator('input[id="id_password"]').click();
+
+    await page.locator('input[id="id_password"]').fill(process.env.FREESOUND_PASSWORD);
+
+    await page.locator('form input[value="login"]').click();
+
+    await expect(page).toHaveURL('https://freesound.org/');
+  }
+
+  let savedItems = readFile(path.join(process.env.TRIAGE_PATH, '..', 'failedItems.json'));
+
+  if (savedItems !== null && savedItems.length) {
+    console.log('has saved items');
+
+    for (let j = 0; j < savedItems.length; j += 1) {
+      const item = savedItems[j];
+      const { tag } = item;
+
+      const result = await downloadItem({
+        context, item, tag,
+      });
+
+      if (result === 'error') {
+        console.log('hit limit');
+        savedItems = savedItems.slice(j);
+        writeFile(path.join(process.env.TRIAGE_PATH, '..', 'failedItems.json'), savedItems);
+        return;
+      }
+    }
+
+    writeFile(path.join(process.env.TRIAGE_PATH, '..', 'failedItems.json'), []);
+  } else {
+    const res = await downloadFromPages({ page, context });
+
+    console.log({ res: res.hasHitLimit, si: res.storedItems.length });
+
+    if (res.hasHitLimit) {
+      writeFile(path.join(process.env.TRIAGE_PATH, '..', 'failedItems.json'), res.storedItems);
+    }
+  }
 });
--- src/utils/browser.utils.js
+++ src/utils/browser.utils.js
@@ -1,36 +1,74 @@
 const path = require('path');
+const { URLSearchParams, URL } = require('url');
 const { moveFile } = require('./fs.utils');
 const { getTextOfElement } = require('./playwright-helpers/utils/browser.utils');
 
-const getItemDataArray = async (items, { category, genre }) => {
+const blacklistedTerms = process.env.BLACKLISTED_TERMS.split(',').filter((term) => !!term).map((term) => term.toLocaleLowerCase());
+
+console.log({ blacklistedTerms });
+
+const hasBlacklistedTerm = (item) => {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const term of blacklistedTerms) {
+    if (item.toLocaleLowerCase().includes(term)) {
+      return term;
+    }
+  }
+
+  return false;
+};
+
+const getItemDataArray = async (items, { tag }) => {
   const itemDataArray = [];
   const numberOfItems = await items.count();
 
   for (let i = 0; i < numberOfItems; i += 1) {
     const item = await items.nth(i);
-    const title = await getTextOfElement({ page: item, query: '.player-title' });
-    const author = await getTextOfElement({ page: item, query: '.icon-small.icon-user' });
-    const href = await item.locator('.player-title').getAttribute('href');
-    const id = href.replace('https://www.looperman.com/loops/detail/', '').split('/')[0];
+    const title = await getTextOfElement({ page: item, query: '.title' });
+    const author = await getTextOfElement({ page: item, query: '.user' });
+    let href = await item.locator('.title').getAttribute('href');
 
-    const filename = `${`${id}-${author}-${title}`.replace(/[^a-z0-9]/gi, '_').toLowerCase().slice(0, 100)}.wav`;
+    const hrefSplit = href.split('/');
+    let id = hrefSplit.pop();
 
-    itemDataArray.push({
+    if (!id) {
+      id = hrefSplit.pop();
+    }
+
+    href = `https://freesound.org${href}`;
+
+    const tempItem = {
       id,
       title,
       author,
       href,
-      filename,
-      category,
-      genre,
-    });
+      tag,
+    };
+
+    const forCheck = JSON.stringify(tempItem);
+    const hasBLTerm = hasBlacklistedTerm(forCheck);
+
+    if (hasBLTerm) {
+      console.log(`item ${title} has a blacklisted term ${hasBLTerm} 😢`);
+    } else {
+      const filename = `${`${id}-${author}-${title}`.replace(/[^a-z0-9]/gi, '_').toLowerCase().slice(0, 100)}.wav`;
+
+      itemDataArray.push({
+        id,
+        title,
+        author,
+        href,
+        filename,
+        tag,
+      });
+    }
   }
 
   return itemDataArray;
 };
 
 const downloadItem = async ({
-  context, item, category, genre,
+  context, item, tag,
 }) => {
   const { href, title, filename } = item;
 
@@ -40,8 +78,14 @@ const downloadItem = async ({
 
   await page.goto(href);
 
-  const downloadButtons = await page.locator('.player-wrapper .player-big-btn.btn-download');
-  const downloadButton = await downloadButtons.first();
+  const wariningLink = await page.locator('a#remove_warning_link');
+  const wariningLinkCount = await wariningLink.count();
+
+  if (wariningLinkCount) {
+    await wariningLink.click();
+  }
+
+  const downloadButton = await page.locator('a[title="download sound"]');
 
   try {
     const [download] = await Promise.all([
@@ -58,10 +102,9 @@ const downloadItem = async ({
     if (downloadPath) {
       moveFile(
         downloadPath,
-        path.join(process.env.TRIAGE_PATH, category, genre, filename),
+        path.join(process.env.TRIAGE_PATH, tag, filename),
         [
-          path.join(process.env.TRIAGE_PATH, category),
-          path.join(process.env.TRIAGE_PATH, category, genre),
+          path.join(process.env.TRIAGE_PATH, tag),
         ],
       );
     }
@@ -77,8 +120,29 @@ const downloadItem = async ({
   }
 };
 
+const getItemsInPage = async ({ page, tag }) => {
+  const items = await page.locator('.sample_player_small');
+
+  try {
+    await items.first().waitFor({ state: 'visible', timeout: 3000 });
+
+    const numberOfItems = await items.count();
+
+    console.log({ numberOfItems });
+
+    const itemDataArray = await getItemDataArray(items, { tag });
+
+    return itemDataArray;
+  } catch (error) {
+    console.log('no items were found');
+
+    return [];
+  }
+};
+
 const downloadFromPages = async ({ page, context }) => {
   const pages = process.env.PAGE_URLS.split(',').filter((pageString) => pageString);
+  const pagesToCheckForTag = process.env.PAGES_TO_CHECK_FOR_TAG || 3;
 
   console.log({ pages });
   let storedItems = [];
@@ -90,50 +154,65 @@ const downloadFromPages = async ({ page, context }) => {
 
     await page.goto(currentUrl);
 
-    // const genreFull = await getTextOfElement({ page, query: '.section-title' });
-    // const genre = genreFull.replace('Free ', '').replace(' Drum Music Loops & Samples', '');
+    let tag = 'other';
+    const urlObject = new URL(currentUrl);
+    const { pathname } = urlObject;
 
-    const genreSelect = await page.locator('select[name="gid"]');
+    if (pathname.includes('tag')) {
+      const urlSplit = currentUrl.split('/');
+      tag = urlSplit.pop();
 
-    await genreSelect.waitFor({ state: 'visible' });
-
-    const genreSelectValue = await page.$eval('select[name="gid"]', (sel) => sel.value);
-    const genre = await getTextOfElement({ page: genreSelect, query: `option[value="${genreSelectValue}"]` });
-
-    const categorySelect = await page.locator('select[name="cid"]');
-
-    await categorySelect.waitFor({ state: 'visible' });
-
-    const categorySelectValue = await page.$eval('select[name="cid"]', (sel) => sel.value);
-    const category = await getTextOfElement({ page: categorySelect, query: `option[value="${categorySelectValue}"]` });
+      if (!tag) {
+        tag = urlSplit.pop();
+      }
+    } else {
+      const search = new URLSearchParams(currentUrl);
+      tag = search.get('ffTag');
+    }
 
     console.log({
-      genre, category,
+      tag,
     });
 
-    const items = await page.locator('.jp-audio.jp-state-looped');
+    // go to last page
+    const lastPageLinks = await page.locator('a[title="Last Page"]');
+    await lastPageLinks.nth(0).click();
 
-    try {
-      await items.first().waitFor({ state: 'visible', timeout: 3000 });
-
-      const numberOfItems = await items.count();
-
-      console.log({ numberOfItems });
-
-      const itemDataArray = await getItemDataArray(items, { category, genre });
+    for (let j = 0; j < pagesToCheckForTag; j += 1) {
+      const itemDataArray = await getItemsInPage({ page, tag });
 
       storedItems = [...storedItems, ...itemDataArray];
-    } catch (error) {
-      console.log('no items were found');
+
+      const pagination = await page.locator('.pagination li');
+      const numberOfPages = await pagination.count();
+      let lastPageIndex = numberOfPages - 1;
+      let currentPageIndex = null;
+
+      while (!currentPageIndex) {
+        const currentPage = await pagination.nth(lastPageIndex);
+        const classList = await currentPage.getAttribute('class');
+
+        if (classList.includes('current-page')) {
+          currentPageIndex = lastPageIndex;
+        } else {
+          lastPageIndex -= 1;
+        }
+      }
+
+      await pagination.nth(currentPageIndex - 1).click();
     }
   }
 
+  storedItems = storedItems.filter(
+    (stItem) => storedItems.filter((item) => item.id === stItem.id).length === 1,
+  );
+
   for (let j = 0; j < storedItems.length; j += 1) {
     const item = storedItems[j];
-    const { category, genre } = item;
+    const { tag } = item;
 
     const result = await downloadItem({
-      context, item, category, genre,
+      context, item, tag,
     });
 
     if (result === 'error') {
